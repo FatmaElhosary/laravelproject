b2aa652a3a5c59f4e4974852dcc13b9ba90b35e5
FatmaElhosary <tidli666>
2017-02-27 16:41:21 +0200

    new design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,13 +26,21 @@
         <td>{{$article->title}}</td>
         <td>{{$article->body}}</td>
          <td> fatma </td>
-
+         <td> <input type="button" value="Edit Aricle" /> <input type="button" value="DeleteAricle"/> &nbsp;&nbsp;<input type="checkbox"value="published"/></td>
       </tr>
       @endforeach
-     
     </tbody>
   </table>
+  <form>
+	 <h3>Add new article </h3>
+	 <input id="in" type="text" placeholder="ArticleTitle*"> 
+	 <br></br>
+     <textarea placeholder="ArticleBody*"></textarea>
+	 </br><br>
+         <input type="button" value="Add Aricle" />
+      </form>
 </div>
+         
             
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         </body>
